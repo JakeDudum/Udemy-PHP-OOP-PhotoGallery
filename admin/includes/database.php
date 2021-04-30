66da046d2e15bbe2d028127d9dff709988fc4d37
Jake Dudum <jakedudum@gmail.com>
2021-04-29 20:05:39 -0700

    Modified Database class to use object oriented connection and created functions inside the class to query db, confirm queries, and escape strings.

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -1,21 +1,44 @@
-<?php 
+<?php
 
 require_once("config.php");
 
-class Database {
+class Database
+{
 
     public $connection;
 
-    function __construct() {
+    function __construct()
+    {
         $this->open_db_connection();
     }
 
-    public function open_db_connection() {
-        $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    public function open_db_connection()
+    {
+        $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-        if(mysqli_connect_errno()) {
-            die("Database connection failed badly" . mysqli_error());
+        if ($this->connection->connect_errno) {
+            die("Database connection failed badly" . $this->connection->connect_error);
         }
+    }
+
+    public function query($sql)
+    {
+        $result = $this->connection->query($sql);
+
+        return $result;
+    }
+
+    private function confirm_query($result)
+    {
+        if (!$result) {
+            die("Query Failed") . $this->connection->error;
+        }
+    }
+
+    public function escape_string($string)
+    {
+        $escaped_string = $this->connection->real_escape_string($string);
+        return $escaped_string;
     }
 }
 
