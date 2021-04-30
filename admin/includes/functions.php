a6428cabc4b0ee2dc349d19ff8f6b0b1715add0d
Jake Dudum <jakedudum@gmail.com>
2021-04-29 21:54:22 -0700

    Created functions.php file in admin/includes folder and created function classAutoLoader which uses php built in function spl_autoload_register() to go and include any classes that may be missing an include statement by accident to prevent our app from erroring out.

---
+++ admin/includes/functions.php
@@ -0,0 +1,16 @@
+<?php 
+
+function classAutoLoader($class) {
+    $class = strtolower($class);
+    $the_path = "includes/{$class}.php";
+
+    if(file_exists($the_path)) {
+        require_once($the_path);
+    } else {
+        die("The file named {$class}.php was not found.");
+    }
+}
+
+spl_autoload_register('classAutoLoader');
+
+?>
