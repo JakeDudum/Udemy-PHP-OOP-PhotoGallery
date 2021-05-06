070d8387d450d04e0154a87b9892ceb00b6f3f02
Jake Dudum <jakedudum@gmail.com>
2021-05-05 18:24:58 -0700

    Removed test php code in admin_content.

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -7,25 +7,6 @@
                 Admin
                 <small>Subheading</small>
             </h1>
-            <?php
-
-            // $user = new User();
-
-            // $user->username = "example";
-            // $user->password = "qwerty";
-            // $user->first_name = "John";
-            // $user->last_name = "Smith";
-
-            // $user->create();
-
-            // $user = User::find_user_by_id(3);
-            // $user->last_name = "Doe";
-            // $user->update();
-
-            $user = User::find_user_by_id(3);
-            $user->delete();
-
-            ?>
             <ol class="breadcrumb">
                 <li>
                     <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
