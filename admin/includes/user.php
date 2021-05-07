ab9550c346a68bde7a12202965dd251ee8c96a63
Jake Dudum <jakedudum@gmail.com>
2021-05-06 19:55:26 -0700

    Changed the name of functions in User class.

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -10,12 +10,12 @@ class User
     public $first_name;
     public $last_name;
 
-    public static function find_all_users()
+    public static function find_all()
     {
         return self::find_this_query("SELECT * FROM " . self::$db_table);
     }
 
-    public static function find_user_by_id($user_id)
+    public static function find_by_id($user_id)
     {
         $the_result_array = self::find_this_query("SELECT * FROM " . self::$db_table . " WHERE id = $user_id LIMIT 1");
 
