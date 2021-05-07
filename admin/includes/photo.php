5f46db9c4b0ee841c0dc8bbe2bbb635fcfcb7909
Jake Dudum <jakedudum@gmail.com>
2021-05-07 14:37:55 -0700

    Changed all instances of photo_id to id to make all Db_object functions work for it. Also changed user_id argument in Db_object to id for true abstration of the code. Created delete_photo.php and added delete_photo function to Photo class.

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -4,7 +4,7 @@ class Photo extends Db_object
 {
     protected static $db_table = "photos";
     protected static $db_table_fields = array('title', 'description', 'filename', 'type', 'size');
-    public $photo_id;
+    public $id;
     public $title;
     public $description;
     public $filename;
@@ -51,7 +51,7 @@ class Photo extends Db_object
 
     public function save()
     {
-        if ($this->photo_id) {
+        if ($this->id) {
             $this->update();
         } else {
             if (!empty($this->errors)) {
@@ -78,6 +78,17 @@ class Photo extends Db_object
             } else {
                 $this->errors[] = "The file directory does not have permission";
             }
+        }
+    }
+
+    public function delete_photo()
+    {
+        if ($this->delete()) {
+            $target_path = SITE_ROOT . DS . 'admin' . DS . $this->image_path();
+
+            return unlink($target_path) ? true : false;
+        } else {
+            return false;
         }
     }
 }
--- admin/photos.php
+++ admin/photos.php
@@ -50,8 +50,14 @@ $photos = Photo::find_all();
 
                             foreach ($photos as $photo) : ?>
                                 <tr>
-                                    <td><img src="<?php echo $photo->image_path(); ?>" alt=""></td>
-                                    <td><?php echo $photo->photo_id; ?></td>
+                                    <td><img style="height: 250px; width: 400px;" src="<?php echo $photo->image_path(); ?>" alt="">
+                                        <div class="pictures_link">
+                                            <a href="delete_photo.php/?id=<?php echo $photo->id; ?>">Delete</a>
+                                            <a href="#">Edit</a>
+                                            <a href="#">View</a>
+                                        </div>
+                                    </td>
+                                    <td><?php echo $photo->id; ?></td>
                                     <td><?php echo $photo->filename; ?></td>
                                     <td><?php echo $photo->title; ?></td>
                                     <td><?php echo $photo->size; ?></td>
