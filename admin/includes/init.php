69a4aa91619ec3135e31a790da8d040cc5015f95
Jake Dudum <jakedudum@gmail.com>
2021-05-06 21:16:28 -0700

    Created Photo class and added starter properties to it. Also created SITE_ROOT and INCLUDES_PATH variables in init.php for easier access to both.

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,10 +1,13 @@
 <?php
 
+defined("SITE_ROOT") ? null : define("SITE_ROOT", $_SERVER['DOCUMENT_ROOT'] . '/Udemy-PHP-OOP-PhotGallery');
+
+defined("INCLUDES_PATH") ? null : define("INCLUDES_PATH", SITE_ROOT . '/admin/includes');
+
 require_once("functions.php");
 require_once("config.php");
 require_once("database.php");
 require_once("db_object.php");
 require_once("user.php");
+require_once("photo.php");
 require_once("session.php");
-
-?>
