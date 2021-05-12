edb8be3fa39b3d52d5adbb3306bc1e94264b18f7
Jake Dudum <jakedudum@gmail.com>
2021-05-11 20:06:23 -0700

    Modified admin_content.php to display info on number of page views, user count, photo count, and comment count. Added google pie chart js to header and footer.php and used chart in admin_content. Also added visitor_count method and count property to Session class to track views and added count_all method to Db_object class to return the count for tables in db.

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -1,27 +1,42 @@
 <?php
 
-class Session {
+class Session
+{
 
     private $signed_in = false;
     public $user_id;
     public $message;
+    public $count;
 
-    function __construct() {
+    function __construct()
+    {
         session_start();
+        $this->visitor_count();
         $this->check_the_login();
         $this->check_message();
     }
 
-    public function message($msg="") {
-        if(!empty($msg)) {
+    public function visitor_count()
+    {
+        if (isset($_SESSION['count'])) {
+            return $this->count = $_SESSION['count']++;
+        } else {
+            return $_SESSION['count'] = 1;
+        }
+    }
+
+    public function message($msg = "")
+    {
+        if (!empty($msg)) {
             $_SESSION['message'] = $msg;
         } else {
             return $this->message;
         }
     }
 
-    private function check_message() {
-        if(isset($_SESSION['message'])) {
+    private function check_message()
+    {
+        if (isset($_SESSION['message'])) {
             $this->message = $_SESSION['message'];
             unset($_SESSION['message']);
         } else {
@@ -29,26 +44,30 @@ class Session {
         }
     }
 
-    public function is_signed_in() {
+    public function is_signed_in()
+    {
         return $this->signed_in;
     }
 
-    public function login($user) {
-        if($user) {
+    public function login($user)
+    {
+        if ($user) {
             $this->user_id = $_SESSION['user_id'] = $user->id;
             $this->signed_in = true;
         }
     }
 
-    public function logout() {
+    public function logout()
+    {
         unset($_SESSION['user_id']);
         unset($this->user_id);
         $this->signed_in = false;
     }
 
-    private function check_the_login() {
+    private function check_the_login()
+    {
 
-        if(isset($_SESSION['user_id'])) {
+        if (isset($_SESSION['user_id'])) {
             $this->user_id = $_SESSION['user_id'];
             $this->signed_in = true;
         } else {
@@ -56,7 +75,6 @@ class Session {
             $this->signed_in = false;
         }
     }
-
 }
 
 $session = new Session();
