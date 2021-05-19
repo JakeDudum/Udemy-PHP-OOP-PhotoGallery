3dde66bd3ece2bbd817f03f26bc303be16463c3d
Jake Dudum <jakedudum@gmail.com>
2021-05-19 16:48:00 -0700

    Added method display_sidebar_data to Photo class and used it in ajax_code.php to display the selected photos data in the modal sidebar whenever a photo is clicked on.

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -23,6 +23,19 @@ $(document).ready(function () {
         image_src = $(this).prop('src');
         image_href_splitted = image_src.split("/");
         image_name = image_href_splitted[image_href_splitted.length - 1];
+
+        photo_id = $(this).attr('data');
+
+        $.ajax({
+            url: "includes/ajax_code.php",
+            data: { photo_id: photo_id },
+            type: "POST",
+            success: function (data) {
+                if (!data.error) {
+                    $("#modal_sidebar").html(data);
+                }
+            }
+        });
     });
 
     $("#set_user_image").click(function () {
@@ -35,6 +48,6 @@ $(document).ready(function () {
                     $(".user_image_box a img").prop('src', data);
                 }
             }
-        })
+        });
     });
 });
