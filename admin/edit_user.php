ebef98f61136d9611e81e4432317c01dcdc4bcba
Jake Dudum <jakedudum@gmail.com>
2021-05-19 14:26:52 -0700

    Added initial HTML and CSS styling for photo library modal in edit_user.php which will show all available photos that can be used for user photo.

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -39,6 +39,51 @@ if (empty($_GET['id'])) {
 
 </nav>
 
+<!-- Photo Modal -->
+<div class="modal fade" id="photo-library">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Gallery System Library</h4>
+            </div>
+            <div class="modal-body">
+                <div class="col-md-9">
+                    <div class="thumbnails row">
+
+                        <!-- PHP LOOP HERE CODE HERE-->
+
+                        <div class="col-xs-2">
+                            <a role="checkbox" aria-checked="false" tabindex="0" id="" href="#" class="thumbnail">
+                                <img class="modal_thumbnails img-responsive" src="<!-- PHP LOOP HERE CODE HERE-->" data="<!-- PHP LOOP HERE CODE HERE-->">
+                            </a>
+                            <div class="photo-id hidden"></div>
+                        </div>
+
+                        <!-- PHP LOOP HERE CODE HERE-->
+
+                    </div>
+                </div>
+                <!--col-md-9 -->
+
+                <div class="col-md-3">
+                    <div id="modal_sidebar"></div>
+                </div>
+
+            </div>
+            <!--Modal Body-->
+            <div class="modal-footer">
+                <div class="row">
+                    <!--Closes Modal-->
+                    <button id="set_user_image" type="button" class="btn btn-primary" disabled="true" data-dismiss="modal">Apply Selection</button>
+                </div>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<!-- Photo Modal End-->
+
 <div id="page-wrapper">
 
     <div class="container-fluid">
@@ -52,7 +97,7 @@ if (empty($_GET['id'])) {
                 </h1>
 
                 <div class="col-md-6">
-                    <img class="img-responsive" src="<?php echo $user->get_image(); ?>" alt="">
+                    <a href="#" data-toggle="modal" data-target="#photo-library"><img class="img-responsive" src="<?php echo $user->get_image(); ?>" alt=""></a>
                 </div>
 
                 <form action="" method="post" enctype="multipart/form-data">
