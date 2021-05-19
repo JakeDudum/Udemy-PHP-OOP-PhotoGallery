9305812664469f0f1e2d4aed5723887ba06d2bda
Jake Dudum <jakedudum@gmail.com>
2021-05-19 14:54:01 -0700

    Edited photo_library_modal.php to display all photos in the DB and added listeners to scripts.js to listen for a click on any photo and get information about said photo ready for AJAX call(ajax call is made, but endpoint is not).

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,5 +1,40 @@
 $(document).ready(function () {
+    let user_href;
+    let user_href_splitted;
+    let user_id;
+
+    let image_src;
+    let image_href_splitted;
+    let image_name;
+
     $('#summernote').summernote({
         height: 100
+    });
+
+    $(".modal_thumbnails").click(function () {
+
+        $("#set_user_image").prop('disabled', false);
+        $(this).addClass('selected');
+
+        user_href = $("#user-id").prop('href');
+        user_href_splitted = user_href.split("=");
+        user_id = user_href_splitted[user_href_splitted.length - 1];
+
+        image_src = $(this).prop('src');
+        image_href_splitted = image_src.split("/");
+        image_name = image_href_splitted[image_href_splitted.length - 1];
+    });
+
+    $("#set_user_image").click(function () {
+        $.ajax({
+            url: "include/ajax_code.php",
+            data: { image_name: image_name, user_id: user_id },
+            type: "POST",
+            success: function (data) {
+                if (!data.error) {
+                    alert(image_name);
+                }
+            }
+        })
     });
 });
