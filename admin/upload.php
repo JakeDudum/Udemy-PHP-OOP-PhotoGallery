af0e88b4e6f1a569ae52a3db0edaa83eae25b080
Jake Dudum <jakedudum@gmail.com>
2021-05-19 18:08:53 -0700

    Added some custom styling to dropzone class in dropzone.css and created new row for dropzone form in upload.php. Also adjusted php logic and normal single file upload form in order to be able to handle multi uploads.

--- admin/upload.php
+++ admin/upload.php
@@ -8,10 +8,10 @@
 
 $message = "";
 
-if (isset($_POST['submit'])) {
+if (isset($_FILES['file'])) {
     $photo = new Photo();
     $photo->title = $_POST['title'];
-    $photo->set_file($_FILES['file_upload']);
+    $photo->set_file($_FILES['file']);
 
     if ($photo->save()) {
         $message = "Photo uploaded succesfully";
@@ -45,19 +45,28 @@ if (isset($_POST['submit'])) {
                     Upload
                 </h1>
 
-                <div class="col-md-6">
-                    <?php echo $message; ?>
-                    <form action="upload.php" method="post" enctype="multipart/form-data">
-                        <div class="form-group">
-                            <input class="form-control" type="text" name="title">
-                        </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <?php echo $message; ?>
+                        <form action="upload.php" method="post" enctype="multipart/form-data">
+                            <div class="form-group">
+                                <input class="form-control" type="text" name="title">
+                            </div>
 
-                        <div class="form-group">
-                            <input type="file" name="file_upload">
-                        </div>
+                            <div class="form-group">
+                                <input type="file" name="file">
+                            </div>
 
-                        <input type="submit" name="submit">
-                    </form>
+                            <input type="submit" name="submit">
+                        </form>
+                    </div>
+                </div>
+                <!---End of Row--->
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <form action="upload.php" class="dropzone"></form>
+                    </div>
                 </div>
             </div>
         </div>
